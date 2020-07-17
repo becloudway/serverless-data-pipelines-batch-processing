9cf548d14e37487560bcc56acfe62948957ba392
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-07-17 15:42:22 +0200

    Implemented getting athena query states in batch

--- src/GetAthenaState/handler.py
+++ src/GetAthenaState/handler.py
@@ -4,9 +4,9 @@ athena_client = boto3.client('athena')
 
 
 def handle(event, context):
-    for execution_id in event['QueryExecutionIds']:
-        response = athena_client.get_query_execution(QueryExecutionId=execution_id)
-        state = response['QueryExecution']['Status']['State']
+    response = athena_client.batch_get_query_execution(QueryExecutionIds=event['QueryExecutionIds'])
+    for execution in response['QueryExecutions']:
+        state = execution['Status']['State']
         if state != 'SUCCEEDED':
-            return {'AthenaState': state}
+            return {'AthenaState': state, 'QueryExecutionId': execution['QueryExecutionId']}
     return {'AthenaState': 'SUCCEEDED'}

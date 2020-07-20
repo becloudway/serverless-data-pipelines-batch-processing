3cf0e4401c51cca86bd6586f652c544b1340a65d
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-07-20 10:22:54 +0200

    Added traffic intensities for vehicle classes to ETL

--- queries/InsertETL.sql
+++ queries/InsertETL.sql
@@ -1,5 +1,6 @@
 INSERT INTO "anpr"."sls_data_pipelines_batch_transformed_parquet_athena"
 SELECT uniqueId, recordTimestamp, currentSpeed, bezettingsgraad, previousSpeed, trafficJamIndicator,
+trafficIntensityClass2, trafficIntensityClass3, trafficIntensityClass4, trafficIntensityClass5,
 CASE
 WHEN (currentSpeed - previousSpeed >= 20) THEN 1
 WHEN (currentSpeed - previousSpeed <= -20) THEN -1
@@ -8,7 +9,8 @@ avg(currentSpeed) OVER (PARTITION BY uniqueId ORDER BY minutes ROWS BETWEEN 2 PR
 avg(currentSpeed) OVER (PARTITION BY uniqueId ORDER BY minutes ROWS BETWEEN 10 PRECEDING AND 0 FOLLOWING) AS avgSpeed10Minutes,
 year(originalTimestamp) as year, month(originalTimestamp) as month, day(originalTimestamp) as day, hour(originalTimestamp) as hour FROM
 (
-SELECT uniqueId, recordTimestamp, originalTimestamp, currentSpeed, bezettingsgraad,
+SELECT uniqueId, recordTimestamp, originalTimestamp, currentSpeed, bezettingsgraad, trafficIntensityClass2,
+trafficIntensityClass3, trafficIntensityClass4, trafficIntensityClass5,
 CASE WHEN (currentSpeed BETWEEN 0 AND 40) AND (bezettingsgraad <> 0) THEN 1
 WHEN (currentSpeed BETWEEN 41 AND 250) OR (bezettingsgraad = 0) THEN 0
 ELSE -1 END as trafficJamIndicator,
@@ -19,6 +21,10 @@ SELECT unieke_id as uniqueId,
 from_unixtime(CAST(tijd_waarneming as INTEGER)) as originalTimestamp,
 tijd_waarneming as recordTimestamp,
 (voertuigsnelheid_rekenkundig_klasse2 + voertuigsnelheid_rekenkundig_klasse3 + voertuigsnelheid_rekenkundig_klasse4 + voertuigsnelheid_rekenkundig_klasse5)/4.0 as currentSpeed,
+verkeersintensiteit_klasse2 as trafficIntensityClass2,
+verkeersintensiteit_klasse3 as trafficIntensityClass3,
+verkeersintensiteit_klasse4 as trafficIntensityClass4,
+verkeersintensiteit_klasse5 as trafficIntensityClass5,
 rekendata_bezettingsgraad as bezettingsgraad FROM
 "anpr"."sls_data_pipelines_batch_parquet_destination_parquet")
 WHERE year(originalTimestamp)={year} AND month(originalTimestamp)={month} AND day(originalTimestamp) BETWEEN {start_day} AND {end_day}

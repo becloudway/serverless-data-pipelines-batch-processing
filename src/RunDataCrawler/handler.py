6d35554d616d644c57b44361021ba0e7e237bea7
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-08-26 17:10:40 +0200

    Fix yesterday datetime + remove unused Custom var

--- src/RunDataCrawler/handler.py
+++ src/RunDataCrawler/handler.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 glue_client = boto3.client('glue')
@@ -9,6 +9,6 @@ CRAWLER_NAME = os.environ['CRAWLER_NAME']
 
 def handle(event, context):
     timezone = pytz.timezone('Europe/Brussels')
-    now = datetime.now(timezone)
+    yesterday = datetime.now(timezone) - timedelta(days=1)
     response = glue_client.start_crawler(Name=CRAWLER_NAME)
-    return {'response': response, 'year': event.get('year', now.year), 'month': event.get('month', now.month), 'day': event.get('day', now.day-1)}
+    return {'response': response, 'year': event.get('year', yesterday.year), 'month': event.get('month', yesterday.month), 'day': event.get('day', yesterday.day)}
